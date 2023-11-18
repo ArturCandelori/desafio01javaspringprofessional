104d1c3927054252d30324da5d45c54460930b63
Artur Candelori <artur.candelori@hotmail.com>
2023-11-18 11:57:40 -0300

    Terminal interface

--- src/main/java/com/devsuperior/desafio01/Desafio01Application.java
+++ src/main/java/com/devsuperior/desafio01/Desafio01Application.java
@@ -1,14 +1,42 @@
 package com.devsuperior.desafio01;
 
+import com.devsuperior.desafio01.entities.Order;
+import com.devsuperior.desafio01.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class Desafio01Application {
+public class Desafio01Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio01Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Código: ");
+		int code = sc.nextInt();
+		System.out.print("Valor básico: ");
+		double basic = sc.nextDouble();
+		System.out.print("Porcentagem de desconto: ");
+		double discount = sc.nextDouble();
+
+		Order o1 = new Order(code, basic, discount);
+		System.out.println("Pedido código " + o1.getCode());
+		System.out.printf("Valor total: R$ %.2f", orderService.total(o1));
+
+		sc.close();
+	}
 }
